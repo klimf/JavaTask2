fc46f62dc1722ceb4be893845a571a65b416e446
Клим Фалкенберг <k-f222@ya.ru>
2017-03-19 22:52:44 +0300

    Add add by index method, bug fixes

--- src/LinkedSet.java
+++ src/LinkedSet.java
@@ -9,12 +9,12 @@ public class LinkedSet<E> {
     }
 
     private class Item<T> {
-        public T item;
+        public T value;
         public Item<T> prev;
         public Item<T> next;
 
-        public Item(T item, Item<T> prev, Item<T> next) {
-            this.item = item;
+        public Item(T value, Item<T> prev, Item<T> next) {
+            this.value = value;
             this.prev = prev;
             this.next = next;
         }
@@ -22,41 +22,68 @@ public class LinkedSet<E> {
     }
 
     public void add(E itemToAdd) {
-        /*if(!check(itemToAdd)){
-            throw new IllegalArgumentException();
-        }*/
-        /*System.out.println(String.format(
-                "itemToAdd = %s; \nmain = %s; \n",
-                itemToAdd.toString(),
-                main.toString()
-
-        ));*/
-        Item<E> newItem = new Item<>(itemToAdd, main, main);
+        if(check(itemToAdd)!=-1){
+            throw new IllegalArgumentException(size + ":" + itemToAdd.toString() + " == " + (check(itemToAdd)-1) + ":" + get(check(itemToAdd)-1));
+        }
+        Item<E> newItem = new Item<>(itemToAdd, main.prev, main);
         newItem.prev.next = newItem;
         newItem.next.prev = newItem;
         size++;
     }
 
-    private boolean check(E itemForCheck) {
+    public void add(int index, E itemToAdd) { //here it would be nice to optimise this function by combine check and insert methods (fixme)
+        if(check(itemToAdd)!=-1){
+            throw new IllegalArgumentException(size + ":" + itemToAdd.toString() + " == " + (check(itemToAdd)-1) + ":" + get(check(itemToAdd)-1));
+        }
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index " + index + " isn't in range (0 - " + (size - 1) + ")");
+        }
+        Item<E> item = getItem(index);
+        Item<E> newItem = new Item<>(itemToAdd, item.prev, item);
+        newItem.prev.next = newItem;
+        newItem.next.prev = newItem;
+        size++;
+    }
+
+    private int check(E itemForCheck) {
         Item<E> item = main;
-        for (int i = 0; i < size; i++) {
-            if (item.equals(itemForCheck)){
-                return false;
+        for (int i = 0; i <= size; i++) {
+            if (itemForCheck.equals(item.value)){
+                return i;
             }
             item = item.next;
         }
-        return true;
+        return -1;//is it correct?
+    }
+
+    public Item<E> getItem(int index) {
+        Item<E> item = main;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index " + index + " isn't in range (0 - " + (size - 1) + ")");
+        }
+        for (int i = 0; i <= index; i++) {
+            item = item.next;
+        }
+        return item;
     }
 
     public E get(int index) {
         Item<E> item = main;
         if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index " + index + " isn't in range (0 - " + size + ")");
+            throw new IndexOutOfBoundsException("Index " + index + " isn't in range (0 - " + (size - 1) + ")");
         }
-        for (int i = 0; i < index; i++) {
+        for (int i = 0; i <= index; i++) {
             item = item.next;
         }
-        return item.item;
+        return item.value;
+    }
+
+    public int get(E itemForGetId) {
+        Item<E> item = main;
+        for (int i = 0; i <= size; i++) {
+            item = item.next;
+        }
+        return 5;
     }
 
     public int size() {
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         LinkedSet<String> set = new LinkedSet<>();
+        String s1 = "exception";
         set.add("1");
         set.add("2");
         set.add("3");
+        set.add("4");
+        set.add("5");
+        set.add("6");
 
+        set.add(3, "99");
         for (int i = 0; i < set.size(); i++){
             System.out.println(set.get(i));
         }
